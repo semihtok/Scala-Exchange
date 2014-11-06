8bda98a4eb285747093dff94d7a89aa764ebb503
semihtok <semihtok1@hotmail.com>
2014-11-07 01:55:12 +0200

    adding main structure

---
+++ src/Main.scala
@@ -0,0 +1,25 @@
+//Semih Tok - Basic exchange calculation with Scala
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    
+    // default parameters
+    var source:String = "USD"
+    var target:String = "EUR"
+    var amount = 1
+      
+    var key:String = "your-api-key"
+    
+    
+    var url = "http://currency-api.appspot.com/api/"+ source +"/" + target + ".json?key="+ key
+    var data = scala.io.Source.fromURL(url).mkString
+    
+    var result = scala.util.parsing.json.JSON.parseFull(data)
+    print(result)
+    
+    
+   
+    
+
+  }
+}
