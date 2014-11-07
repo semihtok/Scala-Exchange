f3f33e3abf3f0ce379dae7f87e5e4232a06e0ab0
semihtok <semihtok1@hotmail.com>
2014-11-07 02:05:20 +0200

    Update Main.scala

--- src/Main.scala
+++ src/Main.scala
@@ -7,19 +7,12 @@ object Main {
     var source:String = "USD"
     var target:String = "EUR"
     var amount = 1
-      
-    var key:String = "your-api-key"
-    
-    
+    var key:String = "your-api-key" //Please change here with your API key.
+
     var url = "http://currency-api.appspot.com/api/"+ source +"/" + target + ".json?key="+ key
     var data = scala.io.Source.fromURL(url).mkString
     
     var result = scala.util.parsing.json.JSON.parseFull(data)
-    print(result)
-    
-    
-   
-    
-
+    println(result)
   }
 }
